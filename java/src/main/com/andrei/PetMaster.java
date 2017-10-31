89bd00f3177a72fd7825910a94130f0698bc1e50
Andrei <demon_19888888@mail.ru>
2017-10-31 14:56:10 +0200

    training1

--- java/src/main/com/andrei/PetMaster.java
+++ java/src/main/com/andrei/PetMaster.java
@@ -2,6 +2,11 @@ package com.andrei;
 
 public class PetMaster {
     public static void main(String[] args){
-
+        String petReaction;
+        Pet myPet = new Pet();
+        myPet.eat();
+        petReaction = myPet.say("Чик!! Чирик!!");
+        System.out.println(petReaction);
+        myPet.sleep();
     }
 }
